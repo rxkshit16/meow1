5f6346fcdc659cbeffbdd169ad9ba880eaa7afc7
rxkshit16 <kaushikrakshit16@gmail.com>
2025-08-14 12:41:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ candle.addEventListener('click', () => {
   if (clickCount === 3) {
     candle.classList.add('fade-out');
     setTimeout(() => {
-      candle.src = "https://ibb.co/5hTTWWTt";
+      candle.<imgsrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_BvewkZb8kaS2FXjhJUHij0Cs0zFz4mHEgw&s">;
       candle.classList.remove('fade-out');
     }, 500);
     confetti({ particleCount: 200, spread: 100, origin: { y: 0.6 } });
