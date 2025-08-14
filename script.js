1d8e998783aee476c12e687a01ab003b3f1a9b7c
rxkshit16 <kaushikrakshit16@gmail.com>
2025-08-14 12:42:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,7 @@ const candle = document.getElementById('candle');
 
 candle.addEventListener('click', () => {
   clickCount++;
-  if (clickCount === 3) {
-    candle.classList.add('fade-out');
-    setTimeout(() => {
-      candle.<imgsrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_BvewkZb8kaS2FXjhJUHij0Cs0zFz4mHEgw&s">;
-      candle.classList.remove('fade-out');
-    }, 500);
+  if (clickCount === 3)
     confetti({ particleCount: 200, spread: 100, origin: { y: 0.6 } });
     setTimeout(() => {
       if (confirm("🎁 Click OK for another surprise!")) {
